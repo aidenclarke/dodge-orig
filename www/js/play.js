e3891643fd026506f63d6bdee6111f4b952d53e0
Aiden Clarke <aidinio64@gmail.com>
2016-02-06 11:40:25 -0500

    save

---
+++ www/js/play.js
@@ -0,0 +1,23 @@
+//Dodge Play State
+
+'use strict';
+
+Dodge.Play = function () {};
+
+Dodge.Play.prototype = {
+  
+  init: function () {
+  },
+
+  /*
+  preload: function () {
+  },
+  */
+
+  update: function () {
+  }
+
+
+
+}
+
