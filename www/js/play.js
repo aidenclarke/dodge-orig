f30165be6455d779149a445d7d64b5331a284792
Aiden Clarke <aidinio64@gmail.com>
2016-02-13 11:37:19 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -12,12 +12,16 @@ Dodge.Play.prototype = {
     console.log("onion-san");
   },
 
-  /*
+  
   preload: function () {
+    this.load.image('background','assets/background.png',320,568);
   },
-  */
+
 
   update: function () {
+    this.background = this.add.tileSprite(0,0,320,568,"background");
+    this.background.autoScroll(0,-50);
+    this.background.scale.set(1);
   }
 
 };
