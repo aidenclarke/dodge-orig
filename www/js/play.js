97bb53d82dbcc7264aa92476d09367e430ac52a0
Aiden Clarke <aidinio64@gmail.com>
2016-02-20 11:10:57 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -15,13 +15,22 @@ Dodge.Play.prototype = {
   
   preload: function () {
     this.load.image('background','assets/background.png');
+    this.load.spritesheet('falling','assets/falling.png',46,64,2);
+
   },
 
 
   update: function () {
+    //background that doesnt even scroll idk why 
     this.background = this.add.tileSprite(0,0,320,568,"background");
-    this.background.autoScroll(0,-50);
+    this.background.autoScroll(0,50);
     this.background.scale.set(1);
+    //falling thingy
+    this.falling = this.add.tileSprite(0,0,160,510,"falling");
+    this.falling.anchor.setTo(0.5, 0.5);
+    this.falling.animations.add('turn');
+    this.falling.animations.play('turn');
+
   }
 
 };
