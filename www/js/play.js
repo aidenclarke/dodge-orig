ba3805921962d2b1cf99a36566d6cf1ea5b5d43a
Aiden Clarke <aidinio64@gmail.com>
2016-03-05 11:53:20 -0500

    save

--- www/js/play.js
+++ www/js/play.js
@@ -17,6 +17,10 @@ Dodge.Play.prototype = {
     //Load the sprites
     this.load.image('background','assets/background.png');
     this.load.spritesheet('player','assets/player.png',32,32,4);
+    this.load.image('falling','assets/falling.png',32,32);
+
+    //load audio
+    this.load.audio('music', 'assets/music.mp3');
 
   },
 
@@ -32,14 +36,31 @@ Dodge.Play.prototype = {
     this.player.anchor.setTo(0.5, 0.5);
     this.player.smoothed = false;
     this.player.animations.add('turn');
-    this.player.animations.play('turn',100,true);
+    this.player.animations.play('turn',10,true);
     this.player.scale.set(2);
+
+    //falling thingy
+    this.falling = this.add.sprite(90,200,'falling');
+    this.falling.smoothed = false;
+    this.falling.scale.set(2.5);
+
+    //falling thingy again
+    this.falling2 = this.add.sprite(90,200,'falling');
+    this.falling2.smoothed = false;
+    this.falling2.scale.set(2.5);
 
     //movey key
     this.cursors = game.input.keyboard.createCursorKeys();
+    //Music 
+    this.music = game.add.audio('music');
+  
+    if (1 === 1) {
+      this.music.play();
+    }
   },
 
   update: function () {
+  
     //moving! yipppeeeee
     if (this.cursors.left.isDown) {
       this.player.x -= 5;
@@ -47,14 +68,23 @@ Dodge.Play.prototype = {
     if (this.cursors.right.isDown) {
       this.player.x += 5;
     }
-    if (this.cursors.up.isDown) {
-      this.player.y -= 5;
-    }
-    if (this.cursors.down.isDown) {
-      this.player.y += 5;
+
+    this.falling.y += 10;
+    this.falling2.y +=11;
+
+    if (this.falling.y > 568) {
+      this.falling.x = game.rnd.integerInRange(5, 315);
+      this.falling.y =  -15;
     }
 
+    if (this.falling2.y > 568) {
+      this.falling2.x = game.rnd.integerInRange(5, 315);
+      this.falling2.y = -15;
+    }
+
+
   }
+
 
 };
 
