3ba2c5bffaeaf9982b80f9ee38e11306a4f16823
Aiden Clarke <aidinio64@gmail.com>
2016-03-10 14:48:41 -0500

    save

--- www/js/start.js
+++ www/js/start.js
@@ -12,7 +12,7 @@ Dodge.Start.prototype  = {
 
 
   create: function () {
-    this.scoreText = this.add.text(
+    this.startText = this.add.text(
         this.world.centerX,
         this.world.height/5,
         "",
@@ -23,19 +23,19 @@ Dodge.Start.prototype  = {
         }
     );
 
-    this.scoreText.anchor.setTo(0.5, 0.5);
-    this.scoreText.smoothed = false;
+    this.startText.anchor.setTo(0.5, 0.5);
+    this.startText.smoothed = false;
 
     this.player = this.add.sprite(160,200,'player');
     this.player.scale.set(3);
     this.player.smoothed = false;
     this.player.anchor.setTo(0.5, 0.5);
 
-    this.game.stage.backgroundColor = 0xbada55;
+    this.game.stage.backgroundColor = "#8AE2FF";
   },
 
   update: function () {
-    this.scoreText.setText("TOUCH P\nTO PLAY");
+    this.startText.setText("TOUCH P\nTO PLAY");
 
     if (this.input.keyboard.isDown(Phaser.Keyboard.P)) {
       this.game.state.start('play');
